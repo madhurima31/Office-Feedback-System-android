29898df20f8241ff3a6332f3b8b0e863d7686b55
Madhurima <madhu.mondal3131@gmail.com>
2018-07-02 10:07:12 +0530

    2

---
+++ app/src/main/java/com/example/madhu/inradius/LoginRegister.java
@@ -0,0 +1,34 @@
+package com.example.madhu.inradius;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginRegister extends AppCompatActivity {
+    Button login,register;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+        login=(Button)findViewById(R.id.login);
+        register=(Button)findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginRegister.this,LoginScreen.class);
+                startActivity(i);
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginRegister.this,RegisterScreen.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/madhu/inradius/LoginScreen.java
@@ -0,0 +1,15 @@
+package com.example.madhu.inradius;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LoginScreen extends AppCompatActivity {
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+}
